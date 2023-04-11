99c5f6f0baa5eca19fe0a3cab98f93238a05f04e
Matt Phan <mttphan261@gmail.com>
2023-04-11 16:27:55 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const domNode = document.getElementById("main");
+domNode.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Matt Phan is the champion"
